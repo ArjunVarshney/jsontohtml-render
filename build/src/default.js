5f0f1f20ba16625d4b0d8feba9247a3f45044e0a
Arjun Varshney <varshneyarjun49@gmail.com>
2023-09-08 19:31:48 +0530

    included build folder

---
+++ build/src/default.js
@@ -0,0 +1,47 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.defaultStyles = void 0;
+exports.defaultStyles = {
+    fontSize: '14px',
+    font: "'Lucida Console', monospace",
+    space: '25px',
+    space_from_left: '50px',
+    links: true,
+    colors: {
+        background: 'black',
+        keys: '#d54e50',
+        values: {
+            number: '#FF8811',
+            string: '#b9ba1f',
+            boolean: '#EDA2F2',
+            function: '#FFC43D',
+            undefined: '#06D6A0',
+            null: '#B3B7EE',
+            other: '#FFC43D',
+            curly_brace: '#FFFFFF',
+            square_brace: '#FFFFFF',
+            comma_colon_quotes: '#FFFFFF',
+        },
+    },
+    comments: {
+        show: true,
+        color: 'gray',
+        space_from_left: '35px',
+    },
+    retractors: {
+        show: true,
+        color: '#8c8c8c',
+        space_from_left: '37px',
+    },
+    line_numbers: {
+        show: true,
+        color: '#5c749c',
+        space_from_left: '30px',
+    },
+    bracket_pair_lines: {
+        show: true,
+        color: '#3c3c3c',
+        space_from_left: '6px',
+        type: 'solid',
+    },
+};
